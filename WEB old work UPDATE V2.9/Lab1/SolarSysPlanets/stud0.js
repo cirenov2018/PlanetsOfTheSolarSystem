bd80de795c82ea0b27cf1ab2a2848580734eb060
cirenov2018 <35769075+cirenov2018@users.noreply.github.com>
2023-12-21 13:33:30 +0800

    Add files via upload

---
+++ WEB old work UPDATE V2.9/Lab1/SolarSysPlanets/stud0.js
@@ -0,0 +1,10 @@
+jQuery (document).ready (function functionName() {
+  var
+    degress = 0,
+    timer = setInterval (function () {
+      degress++;
+    //  jQuery ('body').css ('background-image', 'linear-gradient(' + degress + 'deg, #85FFBD 0%, #FFFB7D 100%'
+    )
+
+  }, 60000/360)
+})
